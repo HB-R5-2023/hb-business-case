88f525f6c54e777e78be3096e13734bbad7a3e8d
Lucas Delobelle <lucas@ld-web.net>
2023-11-06 10:05:02 +0100

    :card_file_box: Added User Entity

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $regularUser = new User();
+        $regularUser
+            ->setEmail("lucas@regular.com")
+            ->setPassword($this->hasher->hashPassword($regularUser, "regular"));
+
+        $manager->persist($regularUser);
+
+        $adminUser = new User();
+        $adminUser
+            ->setEmail("lucas@admin.com")
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($this->hasher->hashPassword($adminUser, "admin"));
+
+        $manager->persist($adminUser);
 
         $manager->flush();
     }
